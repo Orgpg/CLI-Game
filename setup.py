75f189fe8c0ebbea296b8f7824b8058ed69271ac
Orgpg <wpappgg@gmail.com>
2025-08-28 14:14:50 +0630

    Fix console script entry point for guessing game

--- setup.py
+++ setup.py
@@ -5,10 +5,11 @@ setup(
     version="0.1",
     packages=find_packages(),
     entry_points={
-        'console_scripts': [
-            'guessing=cli_games.number_guessing_duel:play_game',
-            'mathquiz=cli_games.math_quiz_duel:play_game',
-            'typerace=cli_games.typing_speed_race:play_game',
-        ],
-    },
+    'console_scripts': [
+        'guessing=cli_games.guess_the_number:play_game',
+        'mathquiz=cli_games.math_quiz_duel:play_game',
+        'typerace=cli_games.typing_speed_race:play_game',
+    ],
+},
+
 )
