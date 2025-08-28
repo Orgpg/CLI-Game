0432c4ffa279a4921528dba742ce97b085f22c30
Orgpg <wpappgg@gmail.com>
2025-08-28 14:06:45 +0630

    first commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="cli-games-hnd68",
+    version="0.1",
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'guessing=cli_games.number_guessing_duel:play_game',
+            'mathquiz=cli_games.math_quiz_duel:play_game',
+            'typerace=cli_games.typing_speed_race:play_game',
+        ],
+    },
+)
